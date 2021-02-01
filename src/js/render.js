39f6a713868590660ee55ac06579abfc57b2680f
TMFV <68297974+TMFV@users.noreply.github.com>
2021-02-01 21:02:07 +0200

    build
    build

--- src/js/render.js
+++ src/js/render.js
@@ -1,9 +1,7 @@
 import templ from "../temp.hbs";
 import dishes from '../menu.json';
 
-
 const renderList = document.querySelector('.js-menu');
 const rend = templ(dishes);
-
 
 renderList.insertAdjacentHTML('beforeend', rend);
